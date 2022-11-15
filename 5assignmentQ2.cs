61102fffb4e148885f9efc174a9958a0c76da6ce
ssaattyy <115381381+ssaattyy@users.noreply.github.com>
2022-11-15 10:07:43 +0530

    5 th Assignments

---
+++ 5assignmentQ2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace csassignment5Q1
+{
+    internal class assignQ2
+    {
+        public static void Main()
+        {
+
+
+
+            ArrayList Employee = new ArrayList();
+            Employee.Add(101);
+            Employee.Add("satya");
+            Employee.Add("Trainee");
+            Employee.Add(20000);
+            Employee.Add("Banglore");
+
+
+
+
+
+            foreach (object i in Employee)
+            {
+                Console.WriteLine(i + " ");
+
+
+
+            }
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
---
+++ AssignmentQ3csharp.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace csassignment5Q3
+{
+    internal class Employee
+    {
+
+
+        public int Eid { get; set; }
+        public string EName { get; set; }
+        public string Erole { get; set; }
+        public int Salary { get; set; }
+        public string Location { get; set; }
+
+
+
+
+        public class EmployeeDetails
+        {
+            public static void Main()
+            {
+                List<Employee> Employees = new List<Employee>();
+                Employees.Add(new Employee
+                {
+                    Eid = 101,
+                    EName = "pavani",
+                    Erole = "Trainee",
+                    Location = "Banglore",
+                    Salary = 20000
+                });
+                Employees.Add(new Employee
+                {
+                    Eid = 102,
+                    EName = "Pratyusha",
+                    Erole = "Trainee",
+                    Location = "Banglore",
+                    Salary = 20000
+                });
+                Employees.Add(new Employee
+                {
+                    Eid = 103,
+                    EName = "Amrutha",
+                    Erole = "Trainee",
+                    Location = "Hyderabad",
+                    Salary = 20000
+                });
+
+
+
+
+                foreach (Employee e in Employees)
+                {
+                    Console.WriteLine(e.Eid + " " + e.EName + " " + e.Erole + " " + e.Location + " " + e.Salary);
+                }
+                Console.ReadLine();
+
+
+
+                Console.WriteLine("Total no.of Employees:");
+                Console.WriteLine(Employees.Count());
+                Console.ReadLine();
+
+
+
+                Employee E = Employees.Find(emp => emp.EName.StartsWith("P"));
+                Console.WriteLine("Eid = {0}", E.Eid);
+                Console.ReadLine();
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
